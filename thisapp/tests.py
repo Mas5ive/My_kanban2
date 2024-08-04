05822ee4c93ea929a4e0d19c65b9b3f2344fc135
Mas5ive <empty@empty>
2024-08-04 16:03:02 +0300

    test: add functionality for parametric testing

--- thisapp/tests.py
+++ thisapp/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from functools import wraps
 
-# Create your tests here.
+
+def parametrize(arg_names, arg_values):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args):
+            func(self, *args)
+        wrapper._parametrized = (arg_names, arg_values)
+        return wrapper
+    return decorator
+
+
+class ParametrizedTestCaseMeta(type):
+    """
+    Creates tested methods based on a parametrized method of the Testcase class. They have an index at the end,
+      so you can easily track down errors.
+    In general, this metaclass makes the testing process more reliable, clear and compact.
+    """
+
+    def __new__(cls, name, bases, attrs):
+        new_attrs = {}
+        for attr_name, attr_value in attrs.items():
+            if hasattr(attr_value, '_parametrized'):
+                arg_names, arg_values = attr_value._parametrized
+                for index, values in enumerate(arg_values, 1):
+                    test_method_name = f'{attr_name}_{index}'
+                    new_attrs[test_method_name] = cls.create_test_method(attr_value, values)
+            else:
+                new_attrs[attr_name] = attr_value
+        return super().__new__(cls, name, bases, new_attrs)
+
+    @staticmethod
+    def create_test_method(func, values):
+        @wraps(func)
+        def test_method(self):
+            return func(self, *values)
+        return test_method
+
+
