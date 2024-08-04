0810f37fda44249675a4be12a70803ae15f6c0e6
Mas5ive <empty@empty>
2024-08-04 16:10:50 +0300

    test: add for profile_view

--- thisapp/tests.py
+++ thisapp/tests.py
@@ -1,4 +1,14 @@
 from functools import wraps
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from .forms import CardForm, CommentForm, CreateBoardForm
+from .models import Board, Card, Invitation, Membership
+
+User = get_user_model()
 
 
 def parametrize(arg_names, arg_values):
@@ -36,5 +46,39 @@ class ParametrizedTestCaseMeta(type):
         def test_method(self):
             return func(self, *values)
         return test_method
+
+
+class ProfileViewTestCase(TestCase, metaclass=ParametrizedTestCaseMeta):
+    fixtures = ['data.json']
+
+    def setUp(self):
+        self.username = 'TestUser1'
+        self.password = '321qwe,./'
+        self.client = Client()
+        self.client.login(username=self.username, password=self.password)
+        self.url_view = reverse('profile')
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.url_view)
+        self.assertRedirects(response, f"{reverse('login')}?next={self.url_view}")
+
+    @parametrize('method_name', [['put'], ['post'], ['head'], ['patch'], ['delete']])
+    def test_mot_allowed_http_methods(self, method_name):
+        method = getattr(self.client, method_name)
+        response = method(self.url_view)
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_success_get_http_method(self):
+        response = self.client.get(self.url_view)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'profile.html')
+        self.assertIsInstance(response.context['form'], CreateBoardForm)
+        self.assertEqual(response.context['owner_boards'], [Board.objects.get(title='Wow project')])
+        self.assertEqual(response.context['invitation_boards'], [Board.objects.get(title='Boring project')])
+
+        self.user = User.objects.get(username=self.username)
+        user_invitation = Invitation.objects.filter(user_recipient=self.user).select_related('board', 'user_sender')
+        self.assertQuerySetEqual(response.context['invitations'], user_invitation)
 
 
