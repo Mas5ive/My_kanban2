6313befa575dfb2c4028b2274928f7e14788cf76
Mas5ive <empty@empty>
2024-08-25 14:32:20 +0300

    refactor: restructure static files and templates
    * styles from .html files have been moved to separate .css files
    * common styles and application templates moved to an external directory
      of the project name
    * the project structure became more Django-like

--- my_kanban2/settings.py
+++ my_kanban2/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'my_kanban2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'my_kanban2' / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -154,6 +156,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'my_kanban2' / 'static',
+]
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
